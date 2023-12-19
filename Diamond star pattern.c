0938d7ed854c46be2fceb81f7aa79f53e11734a5
Shafiqul Alam Khan <118207040+sakhan-1111@users.noreply.github.com>
2023-12-19 14:56:20 -0600

    Create Diamond star pattern.c

---
+++ Diamond star pattern.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <conio.h> 
+int main()
+{
+    int i, j, c;
+ 
+    printf("Enter number of columns: ");
+    scanf("%d", &c);
+	
+    for(i=1; i<=c; i++)
+    {
+        for(j=i; j<c; j++)
+        {
+            printf(" ");
+        }
+        for(j=1; j<=((2*i)-1); j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    for(i=c; i>=1; i--)
+    {
+        for(j=i; j<=c; j++)
+        {
+            printf(" ");
+        }
+        for(j=2; j<((2*i)-1); j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    } 
+    return 0;
+} 
